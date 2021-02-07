6b4298e9beefdf0d1069813c937a05768a7493c0
artempenn <agervets197@gmail.com>
2021-02-07 15:27:37 -0500

    working on geolocation

---
+++ src/components/Weather.js
@@ -0,0 +1,11 @@
+const Weather = ({ position }) => {
+    return(
+        <div>
+        <p> long: {position.long}</p>
+        <p>lat: {position.lat}</p>
+      
+        </div>
+    )
+}
+
+export default Weather
