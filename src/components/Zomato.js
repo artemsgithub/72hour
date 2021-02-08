7a2354bd4a5d841d2a604f71cd81acf3ae575afd
Nate Hill <nate.alan.hill@gmail.com>
2021-02-08 12:30:30 -0600

    1230 Monday

--- src/components/Zomato.js
+++ src/components/Zomato.js
@@ -1,6 +1,35 @@
 const zomato = () => {
+    // const zomatoUrl1 = 'https://developers.zomato.com/api/v2.1/geocode?lat=42&lon=4'
+    // const zomatoUrl = `https://developers.zomato.com/api/v2.1/geocode?lat${location.lat}=&lon=${location.lon}`
+    
+    // fetch(zomatoUrl)
+    // .then(result => result.json())
+    // .then(json => {
+    //     console.log(json);
+    //     displayRestaurant(json);
+    //     // displayCatagory(json);
+    // })
+    // function displayRestaurant(json){
+        
+    //     if (json.length === 0) {
+    //         console.log("No Results");
+    //         let restaurant = "null";
+    //     } else {
+            
+    //         let restaurant = json[nearby_restaurant.name];
+    //         console.log(restaurant);
+    //         let type = json[nearby_restaurant.cusine];
+    //         console.log(type);
+    //     }
+    // }
+   
     return (
-        <h1> Food </h1>
+        <div className="restaurantTile">
+<h1> ZOMATO </h1>
+
+
+        </div>
+         
     );
 };
 
