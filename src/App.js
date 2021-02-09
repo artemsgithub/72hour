ba410d96c4026be9e64a8f0ad06bc33ff6afd90c
Nate Hill <nate.alan.hill@gmail.com>
2021-02-08 21:30:49 -0600

    930EST Monday NAte

--- src/App.js
+++ src/App.js
@@ -1,60 +1,52 @@
 import { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom'
 
-import './App.css'; 
+import './App.css';
 import Navbar from './components/Navbar'
 import Weather from './components/Weather'
 import Nasa from './components/Nasa'
-import Food from './components/Zomato'
+import Zomato from './components/Zomato'
 
 // Defining our <App /> component the function name matches the file name
 
- const useLocation = () => {
-  const [location, setLocation] = useState({lat: 0, long: 0})
+function App() {
+  const useLocation = () => {
+    const [location, setLocation] = useState({ lat: 0, long: 0 })
 
-    useEffect(()=>{
-      const success = (position) =>{ 
+    useEffect(() => {
+      const success = (position) => {
         let lat = position.coords.latitude
         let long = position.coords.longitude
-   
-        
+
         console.log(lat, long)
-        setLocation({lat: lat, long: long})
-        
+        setLocation({ lat: lat, long: long })
       }
       navigator.geolocation.getCurrentPosition(success)
-      
-    },[])
+
+    }, [])
 
     return location;
 
-}
+  }
 
-
-function App() {
   // Gets just the
- const location = useLocation()
-    
-  const routes = ['nasa','openweather','zomato']
+  const location = useLocation()
 
-  return ( 
-    <div className="App"> 
+  const routes = ['nasa', 'openweather', 'zomato']
 
-    {/* Establish Navigation for the website */}
+  return (
+    <div className="App">
+
+      {/* Establish Navigation for the website */}
       <Navbar routes={routes} />
       <Route exact path="/nasa"></Route>
 
       <Route exact path="/openweather">
         <Weather position={location} />
       </Route>
-    
 
-      <Route exact path="/zomato">
-
-        <Food position ={location} />
-
-  
-
+      <Route exact path="/Zomato">
+        <Zomato lat={latitude} lon={longitude} />
       </Route>
 
     </div>
